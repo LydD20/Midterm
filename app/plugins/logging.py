d8a0ab400b67a6bec4c8df36d99f92ccb9555a7b
Lydia Daids <lydiadaids7@gmaill.com>
2024-10-22 18:04:46 -0400

    created function to setup logger

--- app/plugins/logging.py
+++ app/plugins/logging.py
@@ -9,22 +9,22 @@ def setup_logger (name="calculator"):
     '''sets logger up based on environment variables'''
 
     #retrieves log level and log path from .env
-    LOG_LEVEL= os.getenv("LOG_LEVEL", "INFO").upper()
+    LOGGER_LEVEL= os.getenv("LOGGER_LEVEL", "INFO").upper()
     LOG_PATH= os.getenv("LOG_PATH", "logs/app.log")
 
     # Creates logger
     logger = logging.getLogger(name)
-    logger.setLevel(LOG_LEVEL)
+    logger.setLevel(LOGGER_LEVEL)
 
     #Checks if handlers have been added to avoid duplicates
     if not logger.hasHandlers():
         #creates file handler for log messages to file specified
         file_handler= logging.FileHandler(LOG_PATH)
-        file_handler.setLevel(LOG_LEVEL)
+        file_handler.setLevel(LOGGER_LEVEL)
 
         #creates console handler to ouput logs
         console_handler = logging.StreamHandler()
-        console_handler.setLevel(LOG_LEVEL)
+        console_handler.setLevel(LOGGER_LEVEL)
 
         #define logging format
         formatter= logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
